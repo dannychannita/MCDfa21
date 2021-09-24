2d69d0927dc2dc75555dd1673dc0d97404bf1d1e
Danny Channita <danny.channita@snhu.edu>
2021-09-24 10:56:33 -0400

    added heroku...

--- PagesAppxCh3xIT221xDjango/settings.py
+++ PagesAppxCh3xIT221xDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k($ttm&hq!mf95peulrim*xtsn5+*a561bq44kchl7ns2v*!4o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
