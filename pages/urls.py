1c9d78f82e106597abf23fabfbdf1820e907f2fc
Danny Channita <dannychannita@yahoo.com>
2021-12-13 13:19:14 -0500

    added lines to make the base.html properly view.

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView #new
+from .views import HomePageView, AboutPageView
+from .views import ReviewsPageView #new
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
+    path('', ReviewsPageView.as_view(), name='reviews'),
 ]
